0c7e28705550082b4903ad980b128bb43dbcf86b
Gourav Rathore <rathore.tanu114@gmail.com>
2026-04-24 11:42:38 +0000

    added stg_orders.sql

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,19 @@
+select 
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordersellingprice,
+o.ordercostprice
+c.customername,
+c.segment,
+c.country,
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }}  o
+left join {{ ref('raw_customer') }}  c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }}  p
+on o.productid = p.productid
